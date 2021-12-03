ab75f65f90308b35ddc1dff15b2729e693f8e425
MuskanGuta1503 <gupta.muskan503@gmail.com>
2021-12-03 01:11:39 -0500

    change

--- VehicleParking/settings.py
+++ VehicleParking/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k#ml=nk$b4j%qb036=193wx^o%bhy58+z&(w%)d!%vx^fp*ob$'
+# SECRET_KEY = 'k#ml=nk$b4j%qb036=193wx^o%bhy58+z&(w%)d!%vx^fp*ob$'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["park-my-vehicle-123.herokuapp.com","localhost"]
 
 
 # Application definition
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=BASE_DIR/'static'
